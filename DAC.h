0d8f55d234091876ee2c652ddd767688bb4f6598
Kairi Rodriguez <136514993+AnonCloudx@users.noreply.github.com>
2026-06-19 22:18:23 +0000

    add dac.h

---
+++ DAC.h
@@ -0,0 +1,26 @@
+// DAC.h
+// This software configures DAC output
+// Hardware connections
+
+#ifndef DAC_H
+#define DAC_H
+#include <stdint.h>
+
+// Header files contain the prototypes for public functions
+// this file explains what the module does
+
+// **************DAC_Init*********************
+// Initialize 4-bit DAC, called once 
+// Input: none
+// Output: none
+void DAC_Init(void);
+
+
+// **************DAC_Out*********************
+// output to DAC
+// Input: 4-bit data, 0 to 15 
+// Input=n is converted to n*3.3V/15
+// Output: none
+void DAC_Out(uint8_t data);
+
+#endif
